67681a734e540f2f79b8e5478cb6a2c95c8edd04
Sunhoo Park <tjsgn07@gmail.com>
2023-05-13 22:05:23 +0900

    Initial Commit

---
+++ mfios/mfiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mfiosApp.swift
+//  mfios
+//
+//  Created by SunHoo Park on 2023/05/13.
+//
+
+import SwiftUI
+
+@main
+struct mfiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
